607d27b8b51d5994e4d8a46bdd2f3f46b92e1bac
summer <848729185@qq.com>
2020-10-17 12:33:38 +0800

    选做1，分析字节码

---
+++ Week_01/HelloByte.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class HelloByte {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        for(int i = 0;i < 10;i++){
+            a += i;
+            b += i;
+            c = a + b;
+            if (c == 32){
+                c = a * b;
+                break;
+            }
+        }
+        System.out.println(c);
+    }
+}
