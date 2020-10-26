6202025900a11d4b6387158f7829f17a4d1fa74f
summer <848729185@qq.com>
2020-10-26 17:13:13 +0800

    GC实验总结报告

--- Week_01/HelloByte.java
+++ Week_01/HelloByte.java
@@ -5,7 +5,7 @@ public class HelloByte {
         int a = 1;
         int b = 1;
         int c = 1;
-        for(int i = 0;i < 10;i++){
+        for(int i = 0;i < 10000;i++){
             a += i;
             b += i;
             c = a + b;
