26f294a7f508b07e423c2abaed9fe546f87d1824
summer <848729185@qq.com>
2020-11-25 10:00:04 +0800

    第六周作业-表结构设计

---
+++ Week_06/summer_store_结构.sql
@@ -0,0 +1,264 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.7.17-log : Database - summer_store
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`summer_store` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `summer_store`;
+
+/*Table structure for table `summer_cart_item` */
+
+DROP TABLE IF EXISTS `summer_cart_item`;
+
+CREATE TABLE `summer_cart_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` bigint(20) NOT NULL COMMENT '购物车id',
+  `item_id` bigint(20) NOT NULL COMMENT '商品id',
+  `num` int(10) DEFAULT '1' COMMENT '商品数量',
+  `status` int(4) DEFAULT '1' COMMENT '状态。1-正常，2-删除',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `summer_collect_item` */
+
+DROP TABLE IF EXISTS `summer_collect_item`;
+
+CREATE TABLE `summer_collect_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` bigint(20) NOT NULL COMMENT '购物车id',
+  `item_id` bigint(20) NOT NULL COMMENT '商品id',
+  `title` varchar(200) DEFAULT NULL COMMENT '商品标题',
+  `price` double DEFAULT NULL COMMENT '商品价格',
+  `pic_path` varchar(200) DEFAULT NULL COMMENT '商品图片路径',
+  `item_param_data` varchar(200) DEFAULT NULL COMMENT '商品参数规格',
+  `status` int(4) DEFAULT '1' COMMENT '状态。1-正常，2-删除',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `summer_content` */
+
+DROP TABLE IF EXISTS `summer_content`;
+
+CREATE TABLE `summer_content` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NOT NULL COMMENT '内容类目ID',
+  `title` varchar(200) DEFAULT NULL COMMENT '内容标题',
+  `sub_title` varchar(100) DEFAULT NULL COMMENT '子标题',
+  `title_desc` varchar(500) DEFAULT NULL COMMENT '标题描述',
+  `url` varchar(500) DEFAULT NULL COMMENT '链接',
+  `pic` varchar(300) DEFAULT NULL COMMENT '图片绝对路径',
+  `pic2` varchar(300) DEFAULT NULL COMMENT '图片2',
+  `content` text COMMENT '内容',
+  `status` int(4) DEFAULT '1' COMMENT '状态，1-正常，2-删除',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  KEY `updated` (`updated`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `summer_content_category` */
+
+DROP TABLE IF EXISTS `summer_content_category`;
+
+CREATE TABLE `summer_content_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '类目ID',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父类目ID=0时，代表的是一级的类目',
+  `name` varchar(50) DEFAULT NULL COMMENT '分类名称',
+  `status` int(4) DEFAULT '1' COMMENT '状态。可选值:1(正常),2(删除)',
+  `sort_order` int(4) DEFAULT NULL COMMENT '排列序号，表示同级类目的展现次序，如数值相等则按名称次序排列。取值范围:大于零的整数',
+  `is_parent` tinyint(1) DEFAULT '1' COMMENT '该类目是否为父类目，1为true，0为false',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`,`status`) USING BTREE,
+  KEY `sort_order` (`sort_order`)
+) ENGINE=InnoDB AUTO_INCREMENT=149 DEFAULT CHARSET=utf8 COMMENT='内容分类';
+
+/*Table structure for table `summer_item` */
+
+DROP TABLE IF EXISTS `summer_item`;
+
+CREATE TABLE `summer_item` (
+  `id` bigint(20) NOT NULL COMMENT '商品id，同时也是商品编号',
+  `cid` bigint(10) NOT NULL COMMENT '所属类目，叶子类目',
+  `brand` varchar(50) DEFAULT NULL COMMENT '品牌',
+  `model` varchar(50) DEFAULT NULL COMMENT '型号',
+  `title` varchar(100) DEFAULT NULL COMMENT '商品标题',
+  `sell_point` varchar(500) DEFAULT NULL COMMENT '商品卖点',
+  `price` double DEFAULT NULL COMMENT '商品价格，单位为：分',
+  `num` int(10) DEFAULT NULL COMMENT '库存数量',
+  `barcode` varchar(30) DEFAULT NULL COMMENT '商品条形码',
+  `image` varchar(500) DEFAULT NULL COMMENT '商品图片',
+  `status` tinyint(4) DEFAULT '2' COMMENT '商品状态，1-正常，2-下架，3-删除',
+  `created` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `cid` (`cid`),
+  KEY `status` (`status`),
+  KEY `updated` (`updated`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+/*Table structure for table `summer_item_cat` */
+
+DROP TABLE IF EXISTS `summer_item_cat`;
+
+CREATE TABLE `summer_item_cat` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '类目ID',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父类目ID=0时，代表的是一级的类目',
+  `name` varchar(50) DEFAULT NULL COMMENT '类目名称',
+  `status` int(1) DEFAULT '1' COMMENT '状态。可选值:1(正常),2(删除)',
+  `sort_order` int(4) DEFAULT NULL COMMENT '排列序号，表示同级类目的展现次序，如数值相等则按名称次序排列。取值范围:大于零的整数',
+  `is_parent` tinyint(1) DEFAULT '1' COMMENT '该类目是否为父类目，1为true，0为false',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`,`status`) USING BTREE,
+  KEY `sort_order` (`sort_order`)
+) ENGINE=InnoDB AUTO_INCREMENT=1183 DEFAULT CHARSET=utf8 COMMENT='商品类目';
+
+/*Table structure for table `summer_item_desc` */
+
+DROP TABLE IF EXISTS `summer_item_desc`;
+
+CREATE TABLE `summer_item_desc` (
+  `item_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `item_desc` text COMMENT '商品描述',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品描述表';
+
+/*Table structure for table `summer_item_param` */
+
+DROP TABLE IF EXISTS `summer_item_param`;
+
+CREATE TABLE `summer_item_param` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '流水id',
+  `item_cat_id` bigint(20) NOT NULL COMMENT '商品分类id',
+  `param_data` text COMMENT '参数数据，格式为json格式',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8 COMMENT='商品规则参数';
+
+/*Table structure for table `summer_item_param_item` */
+
+DROP TABLE IF EXISTS `summer_item_param_item`;
+
+CREATE TABLE `summer_item_param_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `item_id` bigint(20) NOT NULL COMMENT '商品id',
+  `item_param_id` bigint(20) NOT NULL COMMENT '商品参数id',
+  `param_data` text COMMENT '参数数据，格式为json格式',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1426 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `summer_order` */
+
+DROP TABLE IF EXISTS `summer_order`;
+
+CREATE TABLE `summer_order` (
+  `order_id` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '订单id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `add_id` bigint(20) NOT NULL COMMENT '地址id',
+  `payment` double DEFAULT NULL COMMENT '实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分',
+  `payment_type` int(2) DEFAULT NULL COMMENT '支付类型，1、在线支付，2、货到付款',
+  `post_fee` double DEFAULT NULL COMMENT '邮费。精确到2位小数;单位:元。如:200.07，表示:200元7分',
+  `status` int(4) DEFAULT NULL COMMENT '状态：1、未付款，2、已付款，3、未发货，4、已发货，5、待收货，6、待评价，7、交易成功，8、交易关闭，9、删除',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '订单创建时间',
+  `update_time` timestamp NULL DEFAULT NULL COMMENT '订单更新时间',
+  `payment_time` timestamp NULL DEFAULT NULL COMMENT '付款时间',
+  `consign_time` timestamp NULL DEFAULT NULL COMMENT '发货时间',
+  `end_time` timestamp NULL DEFAULT NULL COMMENT '交易完成时间',
+  `close_time` timestamp NULL DEFAULT NULL COMMENT '交易关闭时间',
+  `shipping_name` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '物流名称',
+  `shipping_code` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '物流单号',
+  `buyer_message` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '买家留言',
+  `buyer_nick` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '买家昵称',
+  `buyer_rate` int(2) DEFAULT NULL COMMENT '买家是否已经评价',
+  PRIMARY KEY (`order_id`),
+  KEY `create_time` (`create_time`),
+  KEY `buyer_nick` (`buyer_nick`),
+  KEY `status` (`status`),
+  KEY `payment_type` (`payment_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Table structure for table `summer_order_item` */
+
+DROP TABLE IF EXISTS `summer_order_item`;
+
+CREATE TABLE `summer_order_item` (
+  `id` varchar(20) COLLATE utf8_bin NOT NULL,
+  `item_id` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '商品id',
+  `order_id` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '订单id',
+  `num` int(10) DEFAULT NULL COMMENT '商品购买数量',
+  `title` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '商品标题',
+  `price` double DEFAULT NULL COMMENT '商品单价',
+  `total_fee` double DEFAULT NULL COMMENT '商品总金额',
+  `pic_path` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '商品图片地址',
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  KEY `order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Table structure for table `summer_shipping` */
+
+DROP TABLE IF EXISTS `summer_shipping`;
+
+CREATE TABLE `summer_shipping` (
+  `add_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '地址编号',
+  `user_id` bigint(20) NOT NULL COMMENT '订单ID',
+  `receiver_name` varchar(20) DEFAULT NULL COMMENT '收货人全名',
+  `receiver_phone` varchar(20) DEFAULT NULL COMMENT '固定电话',
+  `receiver_mobile` varchar(30) DEFAULT NULL COMMENT '移动电话',
+  `receiver_state` varchar(10) DEFAULT NULL COMMENT '省份',
+  `receiver_city` varchar(10) DEFAULT NULL COMMENT '城市',
+  `receiver_district` varchar(20) DEFAULT NULL COMMENT '区/县',
+  `receiver_address` varchar(200) DEFAULT NULL COMMENT '收货地址，如：xx路xx号',
+  `receiver_zip` varchar(6) DEFAULT NULL COMMENT '邮政编码,如：310001',
+  `created` timestamp NULL DEFAULT NULL,
+  `updated` timestamp NULL DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '1' COMMENT '地址状态，1-正常，2-删除',
+  `is_default` tinyint(1) DEFAULT NULL COMMENT '是否为默认地址，1-true,0-false',
+  PRIMARY KEY (`add_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `summer_user` */
+
+DROP TABLE IF EXISTS `summer_user`;
+
+CREATE TABLE `summer_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(32) NOT NULL COMMENT '密码，加密存储',
+  `phone` varchar(20) DEFAULT NULL COMMENT '注册手机号',
+  `email` varchar(50) DEFAULT NULL COMMENT '注册邮箱',
+  `created` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`) USING BTREE,
+  UNIQUE KEY `phone` (`phone`) USING BTREE,
+  UNIQUE KEY `email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
